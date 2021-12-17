75b2a511fcad79991c69e42df71c5ac0a1525af5
withsalt <sysrun@163.com>
2021-12-17 21:43:36 +0800

    更新nuget包

--- src/OnceMi.AspNetCore.IdGenerator/IIdGeneratorService.cs
+++ src/OnceMi.AspNetCore.IdGenerator/IIdGeneratorService.cs
@@ -7,8 +7,17 @@ namespace OnceMi.AspNetCore.IdGenerator
 {
     public interface IIdGeneratorService
     {
+        /// <summary>
+        /// 生成一个新的Id
+        /// </summary>
+        /// <returns></returns>
         long NewId();
 
+        /// <summary>
+        /// 生成指定数量的Id
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
         long[] NewIds(int count);
     }
 }
